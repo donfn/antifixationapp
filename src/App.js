850b8cf5156612f6c6176b9de2b61a3b98edba7e
Levi Noeninckx <levi.noeninckx@gmail.com>
2022-04-22 10:47:22 +0200

    add automatic sort update and generate data

--- src/App.js
+++ src/App.js
@@ -11,12 +11,23 @@ class App extends React.Component {
     }
   }
   componentDidMount() {
-    this.interval = setInterval(() => this.getPatients(), 300);
+    //this.interval = setInterval(() => this.getPatients(), 300);
+    this.getPatients();
+    this.interval = setInterval(() => this.generateData(), 1000);
   }
   componentWillUnmount(){
     clearInterval(this.interval)
   }
-
+  generateData() { 
+    this.setState(prevState => {
+      let patients = [...prevState.patients]
+      patients.forEach(patient => {
+        patient.state = Math.random() * 100
+      })
+      return patients;
+      //return patients 
+    })
+  }
   async getPatients() {
     await fetch("http://localhost:3000/patients")
       .then(data => data.json())
@@ -28,7 +39,6 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(this.state.patients)
     return (
       <div className='m-20'>
         <div className='flex space-x-3'>
@@ -37,7 +47,7 @@ class App extends React.Component {
           <p>State</p>
         </div>
         <div className='flex flex-col'>
-          {this.state.patients.map(patient => {
+          {this.state.patients.sort((a,b) => b.state - a.state).map(patient => {
             return <PatientCard
               key={patient.id}
               name={patient.name}
